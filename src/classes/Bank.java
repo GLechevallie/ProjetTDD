dea285a042a6e000903e89c9c2328e574201d7dc
GLechevallie <guillaume.lechevallier@centrale-marseille.fr>
2015-01-19 08:13:08 +0100

    Chapitre 14

--- src/classes/Bank.java
+++ src/classes/Bank.java
@@ -1,11 +1,28 @@
 package classes;
 
+import java.util.Hashtable;
+
 /**
  * Created by Guillaume on 18/01/2015.
  */
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
     
     Money reduce(Expression source, String to){
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+    
+    int rate (String from, String to){
+        if (from.equals(to)){
+            return 1;
+        }
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+    
+    void addRate(String from, String to, int rate){
+        rates.put(new Pair(from, to), new Integer(rate));
+        System.out.println(rates.get(new Pair(from, to))); // Problème ici : la hashtable s'initialise mal. Pourquoi ?
     }
 }
---
+++ src/classes/Pair.java
@@ -0,0 +1,24 @@
+package classes;
+
+/**
+ * Created by Guillaume on 19/01/2015.
+ */
+public class Pair {
+    
+    private String from;
+    private String to;
+    
+    Pair (String from, String to){
+        this.from = from;
+        this.to = to;
+    }
+    
+    public boolean equals(Object object){
+        Pair pair = (Pair) object;
+        return from.equals(pair.from) && to.equals(pair.to);
+    }
+    
+    public int hashcode(){
+        return 0;
+    }
+}
